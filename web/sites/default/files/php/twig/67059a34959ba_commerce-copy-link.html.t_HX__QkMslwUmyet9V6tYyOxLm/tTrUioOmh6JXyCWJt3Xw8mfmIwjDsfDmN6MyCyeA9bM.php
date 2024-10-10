8c5926e671c1f6a28f855a500b70102b217a0870
Your Name <you@example.com>
2024-10-10 14:03:51 +0700

    Theo dõi tất cả các tệp trong repository

---
+++ web/sites/default/files/php/twig/67059a34959ba_commerce-copy-link.html.t_HX__QkMslwUmyet9V6tYyOxLm/tTrUioOmh6JXyCWJt3Xw8mfmIwjDsfDmN6MyCyeA9bM.php
@@ -0,0 +1,101 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/commerce/templates/commerce-copy-link.html.twig */
+class __TwigTemplate_7c96aa8ddd3569c9a27865c7faf7e38e extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 14
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("commerce/copy-link"), "html", null, true);
+        echo "
+";
+        // line 15
+        if (($context["link"] ?? null)) {
+            // line 16
+            echo "  <button type=\"button\" class=\"commerce-copy-link\" data-link=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["link"] ?? null), 16, $this->source), "html", null, true);
+            echo "\" title=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 16, $this->source), "html", null, true);
+            echo "\"></button>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/commerce/templates/commerce-copy-link.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 16,  43 => 15,  39 => 14,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/commerce/templates/commerce-copy-link.html.twig", "/var/www/html/web/modules/contrib/commerce/templates/commerce-copy-link.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 15);
+        static $filters = array("escape" => 14);
+        static $functions = array("attach_library" => 14);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
